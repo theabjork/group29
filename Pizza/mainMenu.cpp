e95d8dde6250e293312296a160942d8337cd4311
nannalop <natalialopezp96@gmail.com>
2017-12-01 12:11:11 +0000

    main and first classes for main project

---
+++ Pizza/mainMenu.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+#include "mainMenu.h"
+using namespace std;
+
+
---
+++ Pizza/mainMenu.h
@@ -0,0 +1,16 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Menu{
+    public:
+            
+
+
+    private:
+
+};
+
+#endif
