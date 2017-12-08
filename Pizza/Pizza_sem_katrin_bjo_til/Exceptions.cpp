889dac7c27cbcd05866c3a0e9ec184027a2e5587
Katrin Arnardottir <Katrin@Katrins-MBP-2.lan>
2017-12-08 20:11:31 +0000

    seee

---
+++ Pizza/Pizza_sem_katrin_bjo_til/Exceptions.cpp
@@ -0,0 +1,9 @@
+//
+//  Exceptions.cpp
+//  Pizza
+//
+//  Created by Katrin Arnardottir on 07/12/2017.
+//  Copyright © 2017 Katrin Arnardottir. All rights reserved.
+//
+
+#include "Exceptions.hpp"
---
+++ Pizza/Pizza_sem_katrin_bjo_til/Exceptions.hpp
@@ -0,0 +1,14 @@
+//
+//  Exceptions.hpp
+//  Pizza
+//
+//  Created by Katrin Arnardottir on 07/12/2017.
+//  Copyright © 2017 Katrin Arnardottir. All rights reserved.
+//
+
+#ifndef Exceptions_hpp
+#define Exceptions_hpp
+
+#include <stdio.h>
+
+#endif /* Exceptions_hpp */
